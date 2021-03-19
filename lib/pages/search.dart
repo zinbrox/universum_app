09a66780ccfd0ee2c670bbbcec705857d9915e46
zinbrox <53508807+zinbrox@users.noreply.github.com>
2021-03-19 10:25:13 +0530

    Added display of search results (not custom)

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -3,10 +3,10 @@ import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
 class Items {
-  //String URL, imageURL, render, rel; // image data
+  String imageURL; // image data
   String center, media_type, description, title, date;
   var keywords;// data
-  Items({this.center, this.media_type, this.description, this.title, this.date, this.keywords});
+  Items({this.imageURL, this.center, this.media_type, this.description, this.title, this.date, this.keywords});
 }
 
 class NASASearch extends StatefulWidget {
@@ -24,26 +24,34 @@ class _NASASearchState extends State<NASASearch> {
     url = "https://images-api.nasa.gov/search?q=pluto";
     var response = await http.get(Uri.parse(url));
     var jsonData = jsonDecode(response.body);
+
     for(var elements in jsonData['collection']['items']) {
-      for(var element in elements['data']) {
-        //print(element);
+      //print(elements['data'][0]['title']);
+      //var responseLink = await http.get(Uri.parse(elements['href']));
+      //var jsonDataLinks = jsonDecode(responseLink.body);
+      //print(jsonDataLinks[0]);
+      print(elements['links'][0]['href']);
         item = Items(
-          center: element['center'],
-          media_type: element['media_type'],
-          description: element['description'],
-          title: element['title'],
-          date: element['date_created'],
-          keywords: element['keywords'],
+          imageURL: elements['links'][0]['href'],
+          center: elements['data'][0]['center'],
+          media_type: elements['data'][0]['media_type'],
+          description: elements['data'][0]['description'],
+          title: elements['data'][0]['title'],
+          date: elements['data'][0]['date_created'],
+          keywords: elements['data'][0]['keywords'],
         );
         searchList.add(item);
-      }
-
     }
   }
   @override
   void initState() {
     super.initState();
     getSearch();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
   @override
   Widget build(BuildContext context) {
@@ -52,7 +60,31 @@ class _NASASearchState extends State<NASASearch> {
         title: Text("NASA Image And Video Library Search"),
         centerTitle: true,
       ),
-      body: Center(child: Text("NASA Search"),),
+      body: ListView.builder(
+          itemCount: searchList.length,
+          itemBuilder: (BuildContext context, int index){
+            return GestureDetector(
+              onTap: (){
+
+              },
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15)
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Image.network(searchList[index].imageURL),
+                    Text(searchList[index].title.toString()),
+                    Text(searchList[index].description.toString()),
+                    Text(searchList[index].date),
+                    Text(searchList[index].keywords.toString()),
+                    Text('Center: ' + searchList[index].center.toString()),
+                    Text(searchList[index].media_type.toString()),
+                  ],
+                ),
+              ),
+            );
+          }),
     );
   }
 }
