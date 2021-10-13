0e6686d2800b43206d14d35191a4a608a29516f3
zinbrox <53508807+zinbrox@users.noreply.github.com>
2021-10-13 22:53:55 +0530

    Update ad_helper.dart

--- lib/helpers/ad_helper.dart
+++ lib/helpers/ad_helper.dart
@@ -4,9 +4,9 @@ class AdHelper {
 
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-7046141522235121/6928242298';
+      return 'ca-app-pub-//YOUR ID HERE';
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-7046141522235121/6928242298';
+      return 'ca-app-pub-//YOUR ID HERE';
     }
     else {
       throw new UnsupportedError('Unsupported platform');
@@ -15,9 +15,9 @@ class AdHelper {
 
   static String get interstitialAdUnitId {
     if (Platform.isAndroid) {
-      return "ca-app-pub-7046141522235121/2250646595";
+      return "ca-app-pub-//YOUR ID HERE";
     } else if (Platform.isIOS) {
-      return "ca-app-pub-7046141522235121/2250646595";
+      return "ca-app-pub-//YOUR ID HERE";
     }
     else {
       throw new UnsupportedError("Unsupported platform");
@@ -26,9 +26,9 @@ class AdHelper {
 
   static String get rewardedAdUnitId {
     if (Platform.isAndroid) {
-      return "ca-app-pub-7046141522235121/3051704322";
+      return "ca-app-pub-//YOUR ID HERE";
     } else if (Platform.isIOS) {
-      return "ca-app-pub-7046141522235121/3051704322";
+      return "ca-app-pub-//YOUR ID HERE";
     }
     else {
       throw new UnsupportedError("Unsupported platform");
